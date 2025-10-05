27d6d3f10146e39b65ee3be8c0b414ea785b9356
Sharukh <buddy2196@hotmail.com>
2025-10-05 12:31:43 +0530

    navbar fix

--- public/js/filter.js
+++ public/js/filter.js
@@ -3,22 +3,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     filterItems.forEach(item => {
         item.addEventListener('click', function(event) {
-            event.preventDefault(); 
-            
+            event.preventDefault();
+
             const category = this.dataset.category;
             const url = new URL(window.location);
-            const categories = url.searchParams.getAll('category');
+            let categories = url.searchParams.getAll('category');
 
             if (category === 'All') {
                 url.searchParams.delete('category');
             } else {
                 const index = categories.indexOf(category);
                 if (index > -1) {
+                    // If the category is already selected, remove it
                     categories.splice(index, 1);
                 } else {
+                    // Otherwise, add it to the selection
                     categories.push(category);
                 }
 
+                // Clear all existing category params and then add the updated ones
                 url.searchParams.delete('category');
                 if (categories.length > 0) {
                     categories.forEach(cat => url.searchParams.append('category', cat));
