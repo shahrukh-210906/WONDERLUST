59c597fce46f7b85f1e48f17b24331c358af99d7
Sharukh <buddy2196@hotmail.com>
2025-10-05 10:57:15 +0530

    wonderlust

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ async function main(){
 const store = MongoStore.create({
   mongoUrl: dbURL,
   crypto:{
-    secret: 'your_secret_key'
+    secret: process.env.SECRET
   },
   touchAfter: 24*3600
 });
@@ -60,7 +60,7 @@ store.on('error', ()=> {
 
 const sessionOptions = {
     store,
-    secret: 'your_secret_key',
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
